5b3dd9c638d843a967e5a0074147383a88fc6063
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-08-01 20:15:29 +0200

    fixed endpoints errors

--- app.py
+++ app.py
@@ -5,87 +5,87 @@ app = Flask(__name__)
 
 @app.route('/cart', methods=['GET', 'PUT'])
 def cart():  # put application's code here
-    return
+    return "/cart"
 
 
 @app.route('/cart/order', methods=['POST'])
 def cart_order():  # put application's code here
-    return
+    return "cart-order"
 
 
 @app.route('/cart/add', methods=['POST'])
 def cart_add():  # put application's code here
-    return
+    return "cart-add"
 
 
 @app.route('/user', methods=['GET', 'POST', 'DELETE'])
 def user():  # put application's code here
-    return
+    return "user"
 
 
 @app.route('/user/register', methods=['POST'])
 def user_register():  # put application's code here
-    return
+    return "user-register"
 
 
 @app.route('/user/sign_in', methods=['POST'])
 def user_sign_in():  # put application's code here
-    return
+    return "user-sign_in"
 
 
 @app.route('/user/logout', methods=['POST'])
 def user_logout():  # put application's code here
-    return
+    return "user-logout"
 
 
 @app.route('/user/restore', methods=['POST'])
 def user_restore():  # put application's code here
-    return
+    return "user-restore"
 
 
 @app.route('/user/orders', methods=['GET'])
 def user_orders_history():  # put application's code here
-    return
+    return "user-orders"
 
 
 @app.route('/user/orders/<order_id>', methods=['GET'])
 def user_order(order_id: int):  # put application's code here
-    return
+    return "user-orders-order_id"
 
 
 @app.route('/user/address', methods=['GET', 'POST'])
 def user_address_list():  # put application's code here
-    return
+    return "user-address"
 
 
 @app.route('/user/address/<address_id>', methods=['GET', 'PUT', 'DELETE'])
 def user_address(address_id: int):  # put application's code here
-    return
+    return "user-address-address_id"
 
 
-@app.route('/menu', methods=['GET'])
+@app.route('/menu', methods=['GET', 'POST'])
 def menu():  # put application's code here
-    return
+    return "menu"
 
 
 @app.route('/menu/<category_name>', methods=['GET'])
 def menu_category(category_name: str):  # put application's code here
-    return
+    return "menu-category_name"
 
 
 @app.route('/menu/<category_name>/<dish_name>', methods=['GET'])
 def menu_dish(category_name: str, dish_name: str):  # put application's code here
-    return
+    return "menu-category_name-dish_name"
 
 
 @app.route('/menu/<category_name>/<dish_name>/review', methods=['POST'])
 def menu_dish_review(category_name: str, dish_name: str):  # put application's code here
-    return
+    return "menu-category_name-dish_name-review"
 
 
 @app.route('/menu/search', methods=['GET'])
 def menu_search():  # put application's code here
-    return
+    return "menu-search"
 
 
 if __name__ == '__main__':
