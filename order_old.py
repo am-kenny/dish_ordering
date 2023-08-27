801c18e6b33857b0b8c820538fff158412c4c25e
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-08-27 02:26:13 +0200

    Created models, changed all functions to SQLAlchemy

--- order_old.py
+++ order_old.py
@@ -7,13 +7,14 @@ from datetime import datetime
 from models import *
 
 
-def add_to_cart(cart: UserOrder, dish_id: str, quantity: int = 1):  # add new items to cart
+def add_to_cart(user_id: int, dish_id: str, quantity: int = 1):  # add new items to cart
+    order_id = user_cart(user_id)
     database.init_db()
-    ordered = database.db_session.query(OrderedDish).where(OrderedDish.order_id == cart.id,
+    ordered = database.db_session.query(OrderedDish).where(OrderedDish.order_id == order_id,
                                                            OrderedDish.dish_id == dish_id).all()
     if not ordered:
         try:
-            new_item = OrderedDish(order_id=cart.id,
+            new_item = OrderedDish(order_id=order_id,
                                    dish_id=dish_id,
                                    dish_quantity=quantity)
             database.db_session.add(new_item)
@@ -22,7 +23,7 @@ def add_to_cart(cart: UserOrder, dish_id: str, quantity: int = 1):  # add new it
             database.db_session.rollback()
             print("Insert Error ")
             print(ex)
-    update_order(cart)
+    update_order(order_id)
 
 
 def user_cart(user_id: int):  # return cart id or create cart and return id
@@ -43,7 +44,7 @@ def user_cart(user_id: int):  # return cart id or create cart and return id
     #         .where(UserOrder.order_status == 0, UserOrder.user_id == user_id).scalar())
 
 
-def update_order(cart: UserOrder):
+def update_order(order_id: int):
     database.init_db()
     # dish_data = database.db_session.query(Dish).join(OrderedDish).where(OrderedDish.order_id == order_id).all()
     sums = database.db_session.query(func.sum(Dish.carbs).label("total_carbs"),
@@ -51,13 +52,28 @@ def update_order(cart: UserOrder):
                                      func.sum(Dish.proteins).label("total_proteins"),
                                      func.sum(Dish.fats).label("total_fats"),
                                      func.sum(Dish.price).label("total_price"),
-                                     ).join(OrderedDish).where(OrderedDish.order_id == cart.id).one()
-    cart.order_carbs = sums.total_carbs
-    cart.order_kcal = sums.total_kcal
-    cart.order_proteins = sums.total_proteins
-    cart.order_fats = sums.total_fats
-    cart.order_price = sums.total_price
-    database.db_session.commit()
+                                     ).join(OrderedDish).where(OrderedDish.order_id == order_id).one()
+
+
+
+    with SQLiteDB("dish.db") as db:
+        dish_data = db.select_from("dish", ["price", "kcal", "proteins", "fats", "carbs", "dish_quantity"],
+                                   {"order_id": order_id}, "ordered_dish", ("dish_id", "id"))
+        sums = {}
+        for dish in dish_data:  # create dict with counted values
+            quantity = dish.pop("dish_quantity")
+            for key, value in dish.items():
+                sums[key] = sums.get(key, 0) + value * quantity
+        insert_sums = {"order_price": sums.get("price"),  # prepare dict for insert
+                       "order_kcal": sums.get("kcal"),
+                       "order_proteins": sums.get("proteins"),
+                       "order_fats": sums.get("fats"),
+                       "order_carbs": sums.get("carbs")}
+        try:
+            db.update_data("user_order", insert_sums, {"id": order_id})
+        except sqlite3.IntegrityError:
+            print("Update Error")
+
 
 def post_order(user_id: int, comment: str):  # change order status to 1 if there are items in cart
     order_id = user_cart(user_id)
